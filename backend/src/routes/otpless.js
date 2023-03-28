29588d84fb94cc1beced8df0476c7aea4cd9b45c
jaishiv <106514172+jaishivkumar@users.noreply.github.com>
2023-03-28 09:31:39 +0530

    done

--- backend/src/routes/otpless.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const OtpLessController = require("../controllers/otpless");
-
-router.post("/getUserDetails", OtpLessController.login);
-
-module.exports = router;
